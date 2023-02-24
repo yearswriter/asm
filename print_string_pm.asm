908ce7a5d7d3ada25cc3c6015a23f9f45fcad3a4
fluffybug <lapshinajane89@mail.ru>
2023-02-24 14:03:22 +0300

    leg work for protected x32

---
+++ print_string_pm.asm
@@ -0,0 +1,23 @@
+[bits 32]
+VIDEO_MEMORY equ 0xb8000
+WHITE_ON_BLACK equ 0x0f
+
+; prints a null terminated string pointed to by EDX
+print_string_pm:
+  pusha
+  mov edx, VIDEO_MEMORY ;set edx to the start of vid mem.
+
+  print_string_pm_loop:
+    mov al, [ebx]           ;Store the char at EBX in AL
+    mov ah, WHITE_ON_BLACK  ; Store the attributes in AH
+    cmp al,0
+    je done
+
+    mov [edx], ax ; Store char and attributes at current character cell
+    add ebx, 1    ; Increment EBX to next char in string
+    add edx, 2    ; Move to next character cell in vid mem
+    jmp print_string_pm_loop
+
+done:
+  popa
+  ret
