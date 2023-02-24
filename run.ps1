a8bbe31fb7e51ba18c506dae80d007b97c90662b
fluffybug <lapshinajane89@mail.ru>
2023-02-24 11:19:28 +0300

    changed to much faster way of testing may be less reliable in actually changing floppy img

--- run.ps1
+++ run.ps1
@@ -1,9 +1,12 @@
 param ([Parameter(Mandatory)]$source_file,$vm_name="asmFun")
-VBoxManage controlvm $vm_name poweroff
-VBoxManage storageattach $vm_name --type=fdd --storagectl='Floppy' --device=0 --medium=none
+#VBoxManage controlvm $vm_name poweroff
+#VBoxManage storageattach $vm_name --type=fdd --storagectl='Floppy' --device=0 --medium=none
+VBoxManage controlvm $vm_name pause
 wsl -e ./build.sh $source_file
-VBoxManage storageattach $vm_name --type=fdd --storagectl='Floppy' --device=0 --medium=./floppy.img
-VBoxManage startvm $vm_name
+VBoxManage controlvm $vm_name resume
+VBoxManage controlvm $vm_name reset
+#VBoxManage storageattach $vm_name --type=fdd --storagectl='Floppy' --device=0 --medium=./floppy.img
+#VBoxManage startvm $vm_name
 VBoxManage debugvm $vm_name getregisters eax
 VBoxManage debugvm $vm_name getregisters ebx
 VBoxManage debugvm $vm_name getregisters ecx
