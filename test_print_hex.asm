117493061260da44bbafce3e01feeb7a713b21c0
fluffybug <lapshinajane89@mail.ru>
2023-02-25 02:15:31 +0300

    added '0x...' formatting

--- test_print_hex.asm
+++ test_print_hex.asm
@@ -1,6 +1,7 @@
 ;
 ; Prints hex value of dx
 ;
+[org 0x7c00] ; Tell the assembler where this code will be loaded
 mov dx , 0x1fb6 ; store the value to print in dx
 call print_hex ; call the function
 
