2a0483238bdcd365ba4387088b597697d2927df4
fluffybug <lapshinajane89@mail.ru>
2023-02-25 22:36:04 +0300

    loops instead of copy-pasta ended up with exactly same amount of code lol. still feels better this way

--- print_hex.asm
+++ print_hex.asm
@@ -3,52 +3,53 @@
 ;
 
 print_hex:
-  ; TODO : manipulate chars at hex_string to reflect DX
-  ; TODO : there is a room for sum loops here to reduce code repetition
-  mov cx, dx
-  and cx, 0xf000 ; f->0
-  ; 0001 1111 1011 0110    AND     1111 0000 0000 0000
-  ; 0001 0000 0000 0000            0001
-  ; fedc|ba98 7654 3210 ->12 SHR-> fedc
-  shr cx, 12 ;4x3
-  call to_char
-  mov [hex_string + 2], cx ; we got '0x' in the result string,
-  ; this is adjustment for that, TODO: can I shift whole string in memory at the very and?
+  pusha ; store registers on stack
 
-  mov cx, dx
-  and cx, 0x0f00 ;f->1
-  ; fedc ba98|7654 3210 ->12 SHR-> 0000 ba98
-  shr cx, 8 ;4x2
-  call to_char
-  mov [hex_string + 3], cx
+  mov bx, hex_string ; storing output string index in bx
+  add bx, 2          ; adjusting for '0x' formatting
 
-  mov cx, dx
-  and cx, 0x00f0 ;f->2
-  shr cx, 4 ; 4x1
-  call to_char
-  mov [hex_string + 4], cx
+  mov al, 12         ; Initial shiftr. value for filtering
+                     ; digits ar pos.: 1111 1111 1111 1111
+                     ; (digit at pos)     12   8    4    0
 
-  mov cx, dx
-  and cx, 0x000f ;f->4
-  ;4x0
-  call to_char
-  mov [hex_string + 5], cx
+  hex_to_str:
+    cmp [bx], word 0 ; if output string current char is 0,
+    je done          ;  we are done, output result
+      mov si, dx     ; storing input value in si
+      mov cl, al     ; setting shiftr value for current iteration
+      shr si, cl     ; shiftr. input value to have the
+                     ; mostright (hex) digit be in the mostright position
+      and si, 0x000f ; acts as filter to the mostright value in si
+      mov cx, si
+      call to_char  ; converts cl hex digit into ASCII code of it
+      mov [bx], cl  ; loads result of the conversion into result string
+; CX is also used here to send hex value from SI
+; to be converted to ASCII value using to_char procedure.
+; Slightly confusing here, since there is no SIL
+; in 16-bit mode, so here we send to CX, but load result from CL.
+; Also, we are overwritng CL value for shiftr. while doing it,
+; which will be set again using AL value at the start of the next loop.
+      inc bx    ; moving to the next char in the output string
+      sub al, 4 ; adjusting shiftr. value for next iteration
+    jmp hex_to_str
+  done:
 
-  mov bx, hex_string
-  call print_string
+  mov bx, hex_string ; loading output string into bx
+  call print_string  ; for print_stringj procedure
+  popa ; restore registers state
   ret
 
 to_char:
-  sub cx, 0xa ; substruct 10d (0xa) to check if it is a digit or letter
+  sub cl, 0xa ; substruct 10d (0xa) to check if it is a digit or letter
   ; checking if we need letter or digit literal
   js digits   ; if it is digit i.e. less then ten,
               ; which results in negative flag ,to digits procedure
-  add cx, 'a' ; if it is a letter, we have relative shift from literal A, and then we need to
+  add cl, 'a' ; if it is a letter, we have relative shift from literal A, and then we need to
   ret         ; add hex value of literal 'A' (or 'a' if we fancy low case),
               ; since they go one after another ex. 0x41='A', 0x42='B', etc.
 
 digits:
-  add cx, '0' + 0xa ; if it is a digit, all we need to do
+  add cl, '0' + 0xa ; if it is a digit, all we need to do
   ret         ; is to add back 0xA, ASCII hex of literal '0' and the number we got,
               ; since they go one after another ex. 0x30='0', 0x31='1', etc.
 
